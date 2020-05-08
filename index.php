2326073b6a194ef110b4139154d8b1520dc05e92
logan <towkrlc@gmail.com>
2020-05-08 11:16:17 +0800

    invoice

---
+++ index.php
@@ -0,0 +1,16 @@
+ <?php //include_once 'base.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php include 'title.php'; ?></title>
+    <link rel="stylesheet" href="css/style.css">
+    <?php include 'CDN.php'; ?>
+</head>
+<body>
+    <?php 
+    include './layout/topbar.php';
+    ?>
+</body>
+</html>
---
+++ layout/topbar.php
@@ -0,0 +1,5 @@
+<div class="container-fluid">
+    <div class="row bg-primary p-2">
+        <div class="topbar col-6  text-white">Header</div>
+    </div>
+</div>
