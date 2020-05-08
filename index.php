93007cee5db71eb1caf4fc8196969c86fc2350dd
logan <towkrlc@gmail.com>
2020-05-08 14:28:00 +0800

    add read me

--- index.php
+++ index.php
@@ -15,5 +15,39 @@ include_once 'DBConnection.php';
     <?php 
     include './layout/header.php';
     ?>
+    <div class="container">
+    <div class="row my-5">
+    <form action="save_invoice.php" method="POST" class='col-12'>
+        <div class="form-group">
+            <label for="period">期別:</label> 
+            <select name="period">
+                <option value="1">1,2月</option>
+                <option value="2">3,4月</option>
+                <option value="3">5,6月</option>
+                <option value="4">7,8月</option>
+                <option value="5">9,10月</option>
+                <option value="6">11,12月</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="year">年分:</label>
+            <select name="year">
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="code">獎號</label>
+            <input nane='code' type="text">
+        </div>
+        <div class="form-group">
+            <label for="cost">花費</label>
+            <input name='cost' type="text">
+        </div>
+        <input type="submit" value="儲存">
+    </form>
+    </div>
+    </div>
 </body>
 </html>
---
+++ save_invoice.php
@@ -0,0 +1,6 @@
+<?php
+    $period = $_POST['period'];
+    $year = $_POST['year'];
+    $code = $_POST['code'];
+    $cost = $_POST['cost'];
+?>
